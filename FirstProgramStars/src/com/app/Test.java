67b0467f7a8a57397e66df133dfc168749c66c43
Prasadgaggera <gvprasad790@gmail.com>
2018-12-31 17:52:28 +0530

    third commit

--- FirstProgramStars/src/com/app/Test.java
+++ FirstProgramStars/src/com/app/Test.java
@@ -4,6 +4,17 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int n=5;
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=i-1;j++)
+			{
+				System.out.print("* ");
+			}
+		System.out.println();
+		}
+		
+		System.out.println("this is github repository");
 	}
 
 }
